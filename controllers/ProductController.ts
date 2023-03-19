77e99c07f9cbb2000d4c68404b2214c8df2d0cf7
anas12136 <kaarwaa@gmail.com>
2023-03-20 03:24:54 +0500

    here is the final commit

--- controllers/ProductController.ts
+++ controllers/ProductController.ts
@@ -1,18 +1,50 @@
 import { ProductModel } from "../models/ProductModel"
 
 export const createProductController= async (req: any,res:any)=>{
+   try {
     const AddProduct = await ProductModel.create({
         title: req.body.title,
         price: req.body.price,
+        image: {
+            data: req.file.buffer,
+            contentType: req.file.mimetype
+        } 
     })
-    res.send( AddProduct)
+    const protuctToShow={
+        id: AddProduct.id,
+        title: AddProduct.title,
+        price: AddProduct.price,
+        image: AddProduct.image?.contentType
+
+    }
+    res.send( protuctToShow)
+   } catch (error:any) {
+    res.status(500).send(error.message)
+   }
 }
 export const deleteProduct = async (req:any, res:any)=>{
     const deleteProduct = await ProductModel.findByIdAndDelete({_id: req.params.id})
     res.send(deleteProduct)
 }
 export const editProduct = async (req:any, res:any)=>{
-    const editProduct = await ProductModel.findByIdAndUpdate({_id: req.params.id},{title: req.body.title, price:req.body.price})
-    const final = editProduct;
-    res.send(final)
+   try {
+    const editProduct = await ProductModel.findByIdAndUpdate(req.params.id,req.body)
+    if(!editProduct){
+        
+     return res.status(404).send('User not found');
+    }
+    res.send(editProduct)
+   } catch (error: any) {
+    res.status(500).send(error.message);
+   }
+    
+} 
+export const DeleteAll = async(req:any,res:any)=>{
+    try {
+        const deleteProducts = await ProductModel.deleteMany()
+        res.send(deleteProducts)
+    } catch (error) {
+        
+    }
+
 }
--- routes/Router.ts
+++ routes/Router.ts
@@ -1,16 +1,23 @@
 import express  from "express";
-import { createProductController, deleteProduct, editProduct } from "../controllers/ProductController";
+import { createProductController, DeleteAll, deleteProduct, editProduct } from "../controllers/ProductController";
 import { HomePage } from "../controllers/HomeController";
+import multer from 'multer'
 
 export const router = express.Router()
 //Home Page
 router.get('/',HomePage)
 
+
+//Multer storage
+const storage = multer.memoryStorage()
+  const upload = multer({ storage: storage });
+
 //Add Product
-router.post('/addProduct',createProductController)
+router.post('/addProduct',upload.single('image.data'),createProductController)
 
 //Delete Product
 router.delete('/deleteProduct/:id',deleteProduct)
+router.delete('/deleteProducts',DeleteAll)
 
 //Edit Product
 router.patch('/editProduct/:id',editProduct)
